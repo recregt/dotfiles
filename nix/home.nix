81af3570d3095f3257765c846d236d2214e5ecbe
Recregt <203110092+recregt@users.noreply.github.com>
2026-03-14 14:38:47 +0200

    migrate dotfiles to home-manager, remove stow

--- nix/home.nix
+++ nix/home.nix
@@ -1,22 +1,9 @@
 { config, pkgs, ... }:
-
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "recregt";
   home.homeDirectory = "/home/recregt";
+  home.stateVersion = "25.11";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     bat
     bottom
@@ -35,11 +22,9 @@
     just
     micro
     mise
-    nushell
     pipx
     procs
     ripgrep
-    stow
     tealdeer
     tokei
     unzip
@@ -48,41 +33,21 @@
     zoxide
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".bash_aliases".source = ../bash/.bash_aliases;
+    ".bashrc".source = ../bash/.bashrc;
+    ".config/starship.toml".source = ../starship/.config/starship.toml;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/recregt/etc/profile.d/hm-session-vars.sh
-  #
+  programs.nushell = {
+    enable = true;
+    configFile.source = ../nushell/config.nu;
+    envFile.source = ../nushell/env.nu;
+  };
+
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "micro";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
